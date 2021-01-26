1cc66439ad2439c2e43cc5678acfe23759643a9c
Pedro Guilherme Oliveira da Silva <pedrooliveira@DESKTOP-50CJBV4.localdomain>
2021-01-25 21:38:48 -0300

    Aplicando TDD na prática

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,30 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import SendForgotPasswordEmail from '@modules/users/services/SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendEmail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmail(
+      fakeUsersRepository,
+      fakeMailProvider
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@example.com',
+    });
+    expect(sendEmail).toHaveBeenCalled();
+  });
+});
