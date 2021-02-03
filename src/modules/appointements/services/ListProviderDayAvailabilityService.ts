522b248e3ad685385e89294681f71cd3e3b8b43c
Pedro Guilherme Oliveira da Silva <pedrooliveira@DESKTOP-50CJBV4.localdomain>
2021-02-03 20:30:58 -0300

    excluindo horários que já passaram

--- src/modules/appointements/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointements/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointements/repositories/IAppoitmentsRepository';
 
@@ -44,14 +44,18 @@ class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currrentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointements.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currrentDate),
       }
     });
     
