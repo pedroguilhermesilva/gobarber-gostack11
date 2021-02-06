597da939403c4a770cee919a2af539666b57f747
Pedro Guilherme Oliveira da Silva <pedrooliveira@DESKTOP-50CJBV4.localdomain>
2021-02-06 17:38:16 -0300

    variáveis ambiente

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,7 @@
 export default {
   jwt: {
-    secret: '9c661b6bf65c1a7aab41f0fcb8c7d296',
+    // secret: '9c661b6bf65c1a7aab41f0fcb8c7d296',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
