63edc177782f1969bc97f00aff2c751efeeace86
Pedro Guilherme Oliveira da Silva <pedrooliveira@DESKTOP-50CJBV4.localdomain>
2021-02-06 16:14:54 -0300

    enviando notificações

--- src/modules/appointements/services/CreateAppointmentService.ts
+++ src/modules/appointements/services/CreateAppointmentService.ts
@@ -1,10 +1,11 @@
-import { isBefore, startOfHour, getHours } from 'date-fns';
+import { isBefore, startOfHour, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointments';
 import IAppointmentsRepository from '../repositories/IAppoitmentsRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 
 interface IRequest {
   provider_id: string;
@@ -17,6 +18,9 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+     @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
   ) {}
 
   public async execute({ provider_id, date, user_id }: IRequest): Promise<Appointment> {
@@ -47,6 +51,13 @@ class CreateAppointmentService {
       user_id,
       date: appointmentDate,
     });
+
+    const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'")
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para dia ${dateFormatted}`
+    })
 
     return appointment;
   }
